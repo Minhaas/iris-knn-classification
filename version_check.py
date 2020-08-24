733fc9782927abb7a8b9ed215c8ed9a58ab132c2
Minhaas <minhaas@Mohammeds-MacBook-Pro.local>
2020-08-25 00:07:46 +0530

    Initial commit on data visulation

---
+++ version_check.py
@@ -0,0 +1,21 @@
+import sys
+print(f"Python version is: {sys.version}")
+
+import numpy as np
+print(f"Numpy version is: {np.__version__}")
+
+import pandas as pd
+print(f"Pandas version is: {sys.version}")
+
+import matplotlib
+print(f"Matplotlib version is: {matplotlib.__version__}")
+
+import scipy
+print(f"Scipy version is: {scipy.__version__}")
+
+import IPython 
+print(f"IPython version is: {IPython.__version__}")
+
+import sklearn 
+print(f"Scikit-learn version is: {sklearn.__version__}")
+
